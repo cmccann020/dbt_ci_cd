71153920cecf3603d16fe9cf12fbdb7f967ff7e8
Callum McCann <cmccann@analytics8.com>
2020-08-08 16:27:37 -0500

    adding final macro

---
+++ macros/snowflake_cd/delete_temp_database.sql
@@ -0,0 +1,20 @@
+{% macro delete_temp_database(temp_db_to_delete) %}
+
+{# /*
+
+Deletes the target temporary database
+
+*/ #}
+
+{% set delete_statement %}
+drop database if exists {{ temp_db_to_delete }}
+{% endset %}
+{{ log('Running: ' ~ delete_statement, info=True) }}
+{%- if temp_db_to_delete in ['test','raw','dev'] pr '_' not in temp_db_to_delete -%}
+    {{ exceptions.raise_compiler_error("Error - cannot delete database " ~ temp_db_to_delete) }}
+{%- else -%}
+    {% do run_query(delete_statement) %}
+{%- endif -%}
+{{ log("Done.", info=True) }}
+
+{% endmacro %}
