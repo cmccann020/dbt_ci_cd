902c18eaeaa49fb0082ceb5c37d8ba3d578e3ab5
Callum McCann <cmccann@analytics8.com>
2020-08-09 11:10:45 -0500

    adding dev back to delete temp db

--- macros/snowflake_cd/delete_temp_database.sql
+++ macros/snowflake_cd/delete_temp_database.sql
@@ -11,7 +11,7 @@ Also changing to add generate database name as opposed to target.database so it 
 drop database if exists {{ generate_database_name() }}
 {% endset %}
 {{ log('Running: ' ~ delete_statement, info=True) }}
-{%- if target.name in ['prod'] or '_' not in '{{ generate_database_name() }}' -%}
+{%- if target.name in ['prod','dev'] or '_' not in '{{ generate_database_name() }}' -%}
     {{ exceptions.raise_compiler_error("Error - cannot delete database " ~ '{{ generate_database_name() }}') }}
 {%- else -%}
     {% do run_query(delete_statement) %}
