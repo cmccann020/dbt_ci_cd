9a1664aaaa8a156ec29f62bcbbda59047e2c8960
Callum McCann <cmccann@analytics8.com>
2020-08-09 10:51:10 -0500

    changes

--- macros/snowflake_cd/delete_temp_database.sql
+++ macros/snowflake_cd/delete_temp_database.sql
@@ -3,15 +3,16 @@
 {# /*
 
 Deletes the target temporary database. CM change to reference target database so removing variable that didn't work for me
+Also changing to add generate database name as opposed to target.database so it doesn't reference dev
 
 */ #}
 
 {% set delete_statement %}
-drop database if exists {{ target.database }}
+drop database if exists {{ generate_database_name() }}
 {% endset %}
 {{ log('Running: ' ~ delete_statement, info=True) }}
-{%- if target.name in ['prod','dev'] or '_' not in target.database -%}
-    {{ exceptions.raise_compiler_error("Error - cannot delete database " ~ target.database) }}
+{%- if target.name in ['prod'] or '_' not in {{ generate_database_name() }} -%}
+    {{ exceptions.raise_compiler_error("Error - cannot delete database " ~ {{ generate_database_name() }}) }}
 {%- else -%}
     {% do run_query(delete_statement) %}
 {%- endif -%}
